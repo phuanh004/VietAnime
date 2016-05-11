4307adc23c9f16d48adffdbf8c474aa3321b106a
phuanh004 <phuanh004@gmail.com>
2016-05-11 19:01:44 +0700

    First commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// jwplayer('my_video').setup({
+//     sources: [{
+//         file: "https://video-sit4-1.xx.fbcdn.net/v/t42.1790-2/12846914_834329666693664_269052571_n.mp4?efg=eyJybHIiOjMwMCwicmxhIjozNzc4LCJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&rl=300&vabr=146&oh=56eb91f0a1d7b2fd6269b2b6b8668bef&oe=5730490A"
+//     },{
+//         file: "https://video-sit4-1.xx.fbcdn.net/v/t43.1792-2/12675636_203990453299469_1831762985_n.mp4?efg=eyJybHIiOjE1MDAsInJsYSI6NDA5NiwidmVuY29kZV90YWciOiJzdmVfaGQifQ%3D%3D&rl=1500&vabr=602&oh=e62a540c68e4cc2a22b2f75f2e73bbea&oe=57304F36"
+//     }]
+// });
+$(".button-collapse").sideNav();
